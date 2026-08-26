509ad1fd3a9b6de12c848dfe889569f9953c86d1
Michael Manabat <manabatmicko123@gmail.com>
2026-08-26 23:16:31 +1200

    refactor: improve main menu display; move menu display to new class MainMenu

---
+++ src/MainMenu.java
@@ -0,0 +1,16 @@
+public class MainMenu {
+    public void showMenu(){
+        System.out.println("=================================================");
+        System.out.println("                   MAIN MENU                     ");
+        System.out.println("=================================================");
+        System.out.println();
+        System.out.println("1. Dashboard");
+        System.out.println();
+        System.out.println("2. Subjects");
+        System.out.println();
+        System.out.println("3. Exit");
+        System.out.println();
+        System.out.print("Select: ");
+    }
+
+}
